d5fea5c100dc67f002ae1ba91191408f22f8c894
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-18 12:50:01 +0000

    Home Component created and initiated

--- src/app/views/app-dashboard/app-dashboard-routing.module.ts
+++ src/app/views/app-dashboard/app-dashboard-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AppDashboardComponent } from './app-dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 let routes: Routes = [
 	{ 
 		path: '', component: AppDashboardComponent,
-		children: []
+		children: [
+            { path: '', pathMatch: 'full', redirectTo: 'home' },
+            { path: 'home', component: HomeComponent },
+		]
 	}
 ];
 
--- src/app/views/app-dashboard/app-dashboard.module.ts
+++ src/app/views/app-dashboard/app-dashboard.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppDashboardComponent } from './app-dashboard.component';
 import { AppDashboardRoutingModule } from './app-dashboard-routing.module';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [AppDashboardComponent],
+  declarations: [AppDashboardComponent, HomeComponent],
   imports: [
     CommonModule,
     AppDashboardRoutingModule
