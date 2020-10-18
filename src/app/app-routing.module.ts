b1e39ce7a0d440acf8e2b15877e724b0707e7994
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-18 12:28:58 +0000

    Main application interface module initiated with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const appDashboardModule = () => import('./views/app-dashboard/app-dashboard.module').then(m => m.AppDashboardModule);
+
+let routes: Routes = [
+	{ path: '', redirectTo: 'app-dashboard', pathMatch: 'full' },
+	{ path: 'app-dashboard', loadChildren: appDashboardModule },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
