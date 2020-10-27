d5c72e00e88f2f2c41d311f7994acb42206e8348
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-27 14:50:57 +0000

    Topnav integrated into the App layout

--- src/app/views/app-dashboard/app-dashboard.module.ts
+++ src/app/views/app-dashboard/app-dashboard.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppDashboardComponent } from './app-dashboard.component';
 import { AppDashboardRoutingModule } from './app-dashboard-routing.module';
 import { HomeComponent } from './home/home.component';
+import { LayoutsModule } from 'src/app/layouts/layouts.module';
 
 
 
@@ -10,7 +14,12 @@ import { HomeComponent } from './home/home.component';
   declarations: [AppDashboardComponent, HomeComponent],
   imports: [
     CommonModule,
-    AppDashboardRoutingModule
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgScrollbarModule,
+    AppDashboardRoutingModule,
+    LayoutsModule
   ]
 })
 export class AppDashboardModule { }
