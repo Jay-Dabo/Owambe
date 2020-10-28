178cda55a28e71869470341df1f7fd083f344bda
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-28 15:52:16 +0000

    HTTP Service initiated into Module

--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -1,2 +1,6 @@
 export * from './user/user.service';
+export * from './category/category.service';
+export * from './donation/donation.service';
+export * from './fundraiser/fundraiser.service';
+export * from './organization/organization.service';
 export * from './token-interceptor/token-interceptor.service';
