75c884a70752e00f3234949c8c87285c5c479c4d
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-28 12:19:15 +0000

    App basic edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SignupComponent } from './user/signup/signup.component';
-import { LoginComponent } from './user/login/login.component';
 
 const appDashboardModule = () => import('./views/app-dashboard/app-dashboard.module').then(m => m.AppDashboardModule);
+const authenticationModule = () => import('./views/authentication/authentication.module').then(m => m.AuthenticationModule);
 
 let routes: Routes = [
 	{ path: '', redirectTo: 'owambe', pathMatch: 'full' },
 	{ path: 'owambe', loadChildren: appDashboardModule },
-	{ path: 'login', component: LoginComponent},
-	{ path: 'signup', component: SignupComponent}
+	{ path: 'authentication', loadChildren: authenticationModule },
 ]
 
 @NgModule({
