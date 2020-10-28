bbb762d76eb4a51854898491e6c1b5584a192103
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-28 12:20:32 +0000

    User module deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontaweso
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +18,6 @@ import { UserModule } from './user/user.module';
     AppRoutingModule,
     FontAwesomeModule,
     NgScrollbarModule,
-    UserModule,
     FormsModule
   ],
   providers: [],
